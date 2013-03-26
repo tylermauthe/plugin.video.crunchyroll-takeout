59852c2dafd21748eedde88555cb77d7f4dd377e
Yury Bubnov <ybubnov@gmail.com>
2013-03-25 20:07:23 -0700

    bumping version

--- resources/lib/crunchy_video.py
+++ resources/lib/crunchy_video.py
@@ -78,20 +78,23 @@ class CrunchyPlayback:
 		return html
 	
 	def startPlayback(self, vid_id, page_url, resolutions, series_name):
-		#print '----------->Start playing '+series_name+' '+page_url
 		stream = {}
 		settings = {}
 		cj = cookielib.LWPCookieJar()
 		settings['username'] = __settings__.getSetting("crunchy_username")
 		settings['password'] = __settings__.getSetting("crunchy_password")
 		settings['quality'] = int(__settings__.getSetting("video_quality"))
+		if __settings__.getSetting("useDroidStream") == 'false':
+			settings['useDroidStream'] = False
+		else:
+			settings['useDroidStream'] = True
 		res_names = ['SD', '480p', '720p', '1080p']
 		res_display = ['p360','p480','p720','p1080']
 		res_format = ['102','106','108']
 		res_quality = ['10','20','21','30','60','61','62','80']
 		x=0
 
-                if (settings['username'] != '' and settings['password'] != ''):
+		if (settings['username'] != '' and settings['password'] != ''):
 			print "CRUNCHYROLL: --> Attempting to log-in with your user account..."
 			url = 'https://www.crunchyroll.com/?a=formhandler'
 			data = urllib.urlencode({'formname':'RpcApiUser_Login', 'next_url':'','fail_url':'/login','name':settings['username'],'password':settings['password']})
@@ -99,7 +102,11 @@ class CrunchyPlayback:
 			COOKIEFILE= os.path.join(self.base_cache_path, "crunchycookie.lwp")
 			self.cookie = cj
 			opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-			opener.addheaders = [('Host','www.crunchyroll.com'),('User-Agent','Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.14) Gecko/20080404 Firefox/2.0.0.14')]
+			if settings['useDroidStream']:
+				opener.addheaders = [('Host', 'www.crunchyroll.com'),('X-Device-Uniqueidentifier', 'ffffffff-931d-1f73-ffff-ffffaf02fa6f'),('X-Device-Manufacturer', 'HTC'),('X-Device-Model', 'HTC Desire'),('X-Application-Name', 'com.crunchyroll.crunchyroid'),('X-Device-Product', 'htc_bravo'),('X-Device-Is-GoogleTV', '0')]
+			else:
+				opener.addheaders = [('Host','www.crunchyroll.com'),('User-Agent','Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.14) Gecko/20080404 Firefox/2.0.0.14')]
+	
 			self.opener = opener
 			print "CRUNCHYROLL: --> Cookie file doesn't exist; saving new one."
 			urllib2.install_opener(opener)
@@ -147,96 +154,115 @@ class CrunchyPlayback:
                                         if isquality is not -1:
                                                 stream['resolution'] = vid_quality
                                                 print "Crunchyroll Takeout -> found " + vid_quality
-	
 		else:
 			print "CRUNCHYROLL: --> No user account found..."
 			opener = urllib2.build_opener()
-			opener.addheaders = [('Host','www.crunchyroll.com'),('User-Agent','Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.14) Gecko/20080404 Firefox/2.0.0.14')]
+			if settings['useDroidStream']:
+				opener.addheaders = [('Host', 'www.crunchyroll.com'),('X-Device-Uniqueidentifier', 'ffffffff-931d-1f73-ffff-ffffaf02fa6f'),('X-Device-Manufacturer', 'HTC'),('X-Device-Model', 'HTC Desire'),('X-Application-Name', 'com.crunchyroll.crunchyroid'),('X-Device-Product', 'htc_bravo'),('X-Device-Is-GoogleTV', '0')]
+			else:	
+				opener.addheaders = [('Host','www.crunchyroll.com'),('User-Agent','Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.14) Gecko/20080404 Firefox/2.0.0.14')]
 			self.opener = opener
 			stream['video_format'] = '102'
 			stream['resolution'] = '10'
-
-                print stream['resolution']
-                print stream['video_format']
-                prhold = {}
-		playlist = "http://www.crunchyroll.com/xml/?req=RpcApiVideoPlayer_GetStandardConfig&media_id="+vid_id+"&video_format="+stream['video_format']+"&video_quality="+stream['resolution']+"&auto_play=1&show_pop_out_controls=1"
-		print "CRUNCHYROLL: --> Playlist is: "+playlist
-		print "CRUNCHYROLL: --> page_url is: "+page_url
-		player_revision = self.checkPlayer(page_url)
-		prhold['Referer'] = "http://static.lln.crunchyroll.com/flash/"+player_revision+"/StandardVideoPlayer.swf"
-		prhold['current_page'] = page_url
-		print "CRUNCHYROLL: --> page_url is: "+player_revision
-		url_ref = urllib.urlencode(prhold)
-		response = self.opener.open(playlist, url_ref)
+		print stream['resolution']
+		print stream['video_format']
+		print settings['useDroidStream']
+		prhold = {}
+		if settings['useDroidStream']:
+			playlist = "http://www.crunchyroll.com/android_rpc/?req=RpcApiAndroid_GetVideoWithAcl&media_id="+vid_id
+			print "CRUNCHYROLL: --> Grab is: "+playlist
+			response = self.opener.open(playlist)
+		else:
+			playlist = "http://www.crunchyroll.com/xml/?req=RpcApiVideoPlayer_GetStandardConfig&media_id="+vid_id+"&video_format="+stream['video_format']+"&video_quality="+stream['resolution']+"&auto_play=1&show_pop_out_controls=1"
+			print "CRUNCHYROLL: --> Playlist is: "+playlist
+			print "CRUNCHYROLL: --> page_url is: "+page_url
+			player_revision = self.checkPlayer(page_url)
+			prhold['Referer'] = "http://static.lln.crunchyroll.com/flash/"+player_revision+"/StandardVideoPlayer.swf"
+			prhold['current_page'] = page_url
+			print "CRUNCHYROLL: --> page_url is: "+player_revision
+			url_ref = urllib.urlencode(prhold)
+			response = self.opener.open(playlist, url_ref)
 		xmlSource = response.read()
 		soup = BeautifulSoup(xmlSource)
-
-		player_url = soup.find('default:chromelessplayerurl').string
-		meta_info = soup.find('media_metadata')
-		try:
-			stream['episode_display'] = series_name + (" - "+meta_info.episode_number.string if meta_info.episode_number.string else '')+(meta_info.episode_title.string+':' if meta_info.episode_title.string else '')
-			stream['series_name'] = series_name
-			stream['series_title'] = meta_info.series_title.string+':' if meta_info.series_title.string else series_name
-			stream['episode_number'] = meta_info.episode_number.string
-			stream['episode_title'] = (meta_info.episode_title.string if meta_info.episode_title.string else stream['episode_display'])
-		except Exception as e:
-			print '--------> Error:'+ str(e)
-			stream['series_title'] = 'Crunchyroll.com'
-		
-		stream_info = soup.find('stream_info')
-		if stream_info:
-			try:
-				stream['url'] = stream_info.host.string
-				if stream_info.host.string is None:
-					stream['url'] = "null.net/dummy"
-				stream['token'] = stream_info.token.string
-				stream['file'] = stream_info.file.string
-				stream['page_url'] = page_url
-				stream['swf_url'] = "http://static.lln.crunchyroll.com/flash/"+player_revision+"/"+player_url
-				try:
-					app = stream['url'].split('.net/')
-					stream['app'] = app[1]
-					print "CRUNCHYROLL: --> App - " + stream['app']
-				except:
-					print "Couldn't find App"
-				
-				useSubs = False
-				mediaid = vid_id
-				subtitles = soup.find('iv')
-				print "CRUNCHYROLL: --> Attempting to find subtitles..."
-				if(subtitles):
-					print "CRUNCHYROLL: --> Found subtitles.  Continuing..."
-					formattedSubs, formatSRT = crunchyDec().returnSubs(xmlSource)
-					if formatSRT:
-						print "CRUNCHYROLL: --> Using unstyled subs!"
-						self.subFormat = '.srt'
-					subfile = open(xbmc.translatePath('special://temp/crunchy_'+ mediaid + self.subFormat), 'w')
-					subfile.write(formattedSubs.encode('utf-8'))
-					subfile.close()
-					useSubs = True
-				else:
-					print "CRUNCHYROLL: --> No subtitles available!"
-					mediaid = ""
-				
-				self.playvideo(stream, mediaid, useSubs)
-			except:
-                                local_string = xbmcaddon.Addon(id='plugin.video.crunchyroll-takeout').getLocalizedString
-                                Video_restricted = local_string(80001)
-                                upsell_msg = local_string(80002)
-                                mature = local_string(80003)
-                                Login_msg = local_string(80004)
-				if stream_info.find('upsell'):
-					if stream_info.upsell.string == '1':
-						ex = 'XBMC.Notification("'+Video_restricted+':","'+upsell_msg+'.", 3000)'
-						xbmc.executebuiltin(ex)
-						print "CRUNCHYROLL: --> Selected video quality is not available to your user account."
-				elif stream_info.find('error'):
-					if stream_info.error.code.string == '4':
-						ex = 'XBMC.Notification("'+mature+':","'+Login_msg+'.", 3000)'
-						xbmc.executebuiltin(ex)
-						print "CRUNCHYROLL: --> This video is marked as Mature Content.  Please login to view it."
+		if settings['useDroidStream']:
+			print xmlSource
+			mp4_url = re.compile(r'"video_url":"(.+?)","h"').findall(xmlSource.replace('\\',''))
+			mp4str = str(mp4_url)
+			mp4str = mp4str.replace('[','')
+			mp4str = mp4str.replace(']','')
+			mp4str = mp4str.replace('\'','')
+			print "CRUNCHYROLL: --> Video_URL is: "+mp4str
+			item = xbmcgui.ListItem('Video presented by Crunchyroll.com')
+			item.setInfo( type="Video", infoLabels={ "Title": 'Video presented by Crunchyroll.com' })
+			item.setProperty('IsPlayable', 'true')
+			xbmc.Player().play(mp4str, item)
 		else:
-			print "Playback Failed!"
+			player_url = soup.find('default:chromelessplayerurl').string
+			meta_info = soup.find('media_metadata')
+			try:
+				stream['episode_display'] = series_name + (" - "+meta_info.episode_number.string if meta_info.episode_number.string else '')+(meta_info.episode_title.string+':' if meta_info.episode_title.string else '')
+				stream['series_name'] = series_name
+				stream['series_title'] = meta_info.series_title.string+':' if meta_info.series_title.string else series_name
+				stream['episode_number'] = meta_info.episode_number.string
+				stream['episode_title'] = (meta_info.episode_title.string if meta_info.episode_title.string else stream['episode_display'])
+			except Exception as e:
+				print '--------> Error:'+ str(e)
+				stream['series_title'] = 'Crunchyroll.com'
+		
+			stream_info = soup.find('stream_info')
+			if stream_info:
+				try:
+					stream['url'] = stream_info.host.string
+					if stream_info.host.string is None:
+						stream['url'] = "null.net/dummy"
+					stream['token'] = stream_info.token.string
+					stream['file'] = stream_info.file.string
+					stream['page_url'] = page_url
+					stream['swf_url'] = "http://static.lln.crunchyroll.com/flash/"+player_revision+"/"+player_url
+					try:
+						app = stream['url'].split('.net/')
+						stream['app'] = app[1]
+						print "CRUNCHYROLL: --> App - " + stream['app']
+					except:
+						print "Couldn't find App"
+					
+					useSubs = False
+					mediaid = vid_id
+					subtitles = soup.find('iv')
+					print "CRUNCHYROLL: --> Attempting to find subtitles..."
+					if(subtitles):
+						print "CRUNCHYROLL: --> Found subtitles.  Continuing..."
+						formattedSubs, formatSRT = crunchyDec().returnSubs(xmlSource)
+						if formatSRT:
+							print "CRUNCHYROLL: --> Using unstyled subs!"
+							self.subFormat = '.srt'
+						subfile = open(xbmc.translatePath('special://temp/crunchy_'+ mediaid + self.subFormat), 'w')
+						subfile.write(formattedSubs.encode('utf-8'))
+						subfile.close()
+						useSubs = True
+					else:
+						print "CRUNCHYROLL: --> No subtitles available!"
+						mediaid = ""
+					
+					self.playvideo(stream, mediaid, useSubs)
+				except:
+					local_string = xbmcaddon.Addon(id='plugin.video.crunchyroll-takeout').getLocalizedString
+					Video_restricted = local_string(80001)
+					upsell_msg = local_string(80002)
+					mature = local_string(80003)
+					Login_msg = local_string(80004)
+					if stream_info.find('upsell'):
+						if stream_info.upsell.string == '1':
+							ex = 'XBMC.Notification("'+Video_restricted+':","'+upsell_msg+'.", 3000)'
+							xbmc.executebuiltin(ex)
+							print "CRUNCHYROLL: --> Selected video quality is not available to your user account."
+					elif stream_info.find('error'):
+						if stream_info.error.code.string == '4':
+							ex = 'XBMC.Notification("'+mature+':","'+Login_msg+'.", 3000)'
+							xbmc.executebuiltin(ex)
+							print "CRUNCHYROLL: --> This video is marked as Mature Content.  Please login to view it."
+			else:
+				print "Playback Failed!"
 		
 	def playvideo(self, stream, mediaid, useSubs):
 		if stream['url'] == "null.net/dummy":
